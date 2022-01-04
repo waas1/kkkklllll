2701614233c82f15f6c00a761fcaa18988e09d59
Erfan Ilyas <repo-bot@waas1.com>
2022-01-04 19:06:20 +0500

    updated hooks

--- waas1-tenant-lifecycle-hooks.php
+++ waas1-tenant-lifecycle-hooks.php
@@ -5,7 +5,7 @@
 /*
 Plugin Name: waas1-tenant-lifecycle-hooks.php
 Plugin URI: https://waas1.com/
-Description: Lifecycle hooks
+Description: Tenant Lifecycle hooks
 Version: 1.0.0
 Author: Erfan
 Author URI: https://waas1.com/
@@ -20,22 +20,77 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 
-function waas1_tenant_lifecycle_created( $args ) {
-	do_action( 'waas1_tenant_created' );
-};
-
-
-
-
-
-function waas1_tenant_lifecycle_pre_delete( $args ) {
-	do_action( 'waas1_tenant_pre_delete' );
-};
-
-
 if ( class_exists( 'WP_CLI' ) ) {
-	WP_CLI::add_command( 'waas1-tenant created', 'waas1_tenant_lifecycle_created' );
-	WP_CLI::add_command( 'waas1-tenant pre-delete', 'waas1_tenant_lifecycle_pre_delete' );
+	
+	WP_CLI::add_command( 'waas1-tenant post-created', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_post_created', $assoc_args );
+	});
+
+	WP_CLI::add_command( 'waas1-tenant pre-delete', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_pre_delete', $assoc_args );
+	});
+	
+	
+	
+	//tenant activate
+	WP_CLI::add_command( 'waas1-tenant pre-activate', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_activate', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-activate', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_activate', $assoc_args );
+	});
+	
+	
+	
+	
+	//tenant deactivate
+	WP_CLI::add_command( 'waas1-tenant pre-deactivate', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_deactivate', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-deactivate', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_deactivate', $assoc_args );
+	});
+	
+	
+	
+	
+	//php version
+	WP_CLI::add_command( 'waas1-tenant pre-php-ver-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_php_ver_change', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-php-ver-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_php_ver_change', $assoc_args );
+	});
+	
+	
+	
+	
+	//wp version
+	WP_CLI::add_command( 'waas1-tenant pre-wp-ver-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_wp_ver_change', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-wp-ver-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_wp_ver_change', $assoc_args );
+	});
+	
+	
+	//restrictions group changed
+	WP_CLI::add_command( 'waas1-tenant pre-restrictions-group-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_restrictions_group_change', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-restrictions-group-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_restrictions_group_change', $assoc_args );
+	});
+	
+	
+	//domain change
+	WP_CLI::add_command( 'waas1-tenant pre-domain-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_pre_domain_change', $assoc_args );
+	});
+	WP_CLI::add_command( 'waas1-tenant post-domain-change', function( $args, $assoc_args ){
+		do_action( 'waas1_tenant_lifecycle_post_domain_change', $assoc_args );
+	});
+	
 }
 
 
